b99c34d7e5444269ec36d7cc1c2adc3faf6e6a8c
Arthur Wambst <wambst.arthur2@gmail.com>
2024-08-29 02:57:37 +0200

    jeu. 29 août 2024 02:57:37 CEST

--- Assets/EnableChildsOnMap.cs
+++ Assets/EnableChildsOnMap.cs
@@ -1,6 +1,7 @@
 using Game;
 using Interfaces;
 using Unity.Netcode;
+using UnityEngine;
 
 
 public class EnableChildsOnMap : NetworkBehaviour, ICallOnSceneChange
@@ -23,11 +24,12 @@ public class EnableChildsOnMap : NetworkBehaviour, ICallOnSceneChange
     public void OnSceneChange(int i)
     {
         //Debug.Log("OnSceneChange to enable childs (" + transform.childCount + " childs)");
-        for (int j = transform.childCount - 1; j >= 0; j--)
-        {
+        //for (int j = transform.childCount - 1; j >= 0; j--)
+        //{
             //Debug.Log("Child " + j + " is active : " + (Variable.SceneCurrent == "Map"));
-            transform.GetChild(j).gameObject.SetActive(Variable.SceneCurrent == "Map");
-        }
+            transform.GetChild(0).transform.GetChild(0).gameObject.SetActive(Variable.SceneCurrent == "Map");
+            transform.GetChild(1).GetComponent<Camera>().enabled = Variable.SceneCurrent == "Map";
+        //}
         
     }
 }
